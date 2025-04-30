25ab515b55b3bb2c310fd22fb6436ec47b1af6d0
Nathan <nathan.agu.2103@gmail.com>
2025-04-30 18:02:03 +0200

    feat: Ajout du routage des pages

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-  <p>Portfolio</p>
+  <header>
+    <p>Header</p>
+  </header>
+  <main>
+    <router-view></router-view>
+  </main>
+  <footer>
+    <p>2025 - Portfolio Nathan AGU</p>
+  </footer>
 </template>
 
 <style scoped>
-
+footer {
+  text-align: center;
+  padding: 10px;
+  background-color: #333;
+  color: white;
+}
 </style>
